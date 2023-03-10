b9b9509cc9f15301a57645e176781916d046dac3
TheoKennel <theokennel@gmail.com>
2023-03-10 22:49:06 +0100

    Ajout de chaque file jsx comportant les sections qui feront la structure de la page. Ajout d'un dossier index.js pour import / export et tous add dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import React from "react";
+
+import { Navbar, Hero,  Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 import styles from "./style.js";
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden" >
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth}`}>
-                NavBar
+                <Navbar />
             </div>
         </div>
 
         <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
-                hero
+                <Hero />
             </div>
         </div>
         <div className={`bg-primary  ${styles.paddingX} ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
-                Stats
-                Business
-                Billing
-                CardDeal
-                Testimonials
-                Clients
-                CTA
-                Footer
+                <Stats />
+                <Business />
+                <Billing />
+                <CardDeal />
+                <Testimonials />
+                <Clients />
+                <CTA />
+                <Footer />
             </div>
         </div>
     </div>
