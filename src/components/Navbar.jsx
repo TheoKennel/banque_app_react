98b4f8c4e06e8e9addd225832401452d712d0a8d
TheoKennel <theokennel@gmail.com>
2023-03-10 23:11:55 +0100

    NavBar début ajout style avec Tailwind, menu 1 ok, reste a faire pour le responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import {useState} from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from "../constants/index.js";
 
 const Navbar = () => {
     return (
-        <div>
+        <nav className="w-full flex py-6 justify-between items-center navbar">
+            <img src={logo} alt="hoobank"
+            className="w-[124px] h-[32px]"/>
+            <ul className="list_none sm:flex hidden justify-end items-center flex-1">
+                {navLinks.map((nav, index) => (
+                    <li
+                        key={nav.id}
+                        className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' :' mr-10'} text-white mr-10`}
+                    >
+                        <a href={`#${nav.id}`}>
+                            {nav.title}
+                        </a>
+                    </li>
 
-        </div>
+
+                ))}
+            </ul>
+        </nav>
     );
 };
 
