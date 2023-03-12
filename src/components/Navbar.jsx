629674237c6122b08ee9a6c3f16bacd72f68257c
TheoKennel <theokennel@gmail.com>
2023-03-12 23:12:42 +0100

    Ajout menu en responsive toggle ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             className="w-[124px] h-[32px]"/>
             <ul className="list_none sm:flex hidden justify-end items-center flex-1">
                 {navLinks.map((nav, index) => (
-                    <li key={nav.id}className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' :' mr-10'} text-white mr-10`} >
+                    <li key={nav.id}  className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' :' mr-10'} text-white mr-10`} >
                         <a href={`#${nav.id}`}> {nav.title} </a>
                     </li>
                 ))}
@@ -21,10 +21,15 @@ const Navbar = () => {
                 <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={() => setToggle((prev) => !prev)}  />
                 <div
                     className={`${
-                        !toggle ? "hidden" : "flex"
-                    } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-                >
+                        !toggle ? "hidden" : "flex"} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}  >
                 </div>
+                <ul className="list_none flex-col justify-end items-center flex-1">
+                    {navLinks.map((nav, index) => (
+                        <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' :' mb-4'} text-white mr-10`} >
+                            <a href={`#${nav.id}`}> {nav.title} </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     );
